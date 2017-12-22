903b7a2c36fd7503e739dd5b3f539f228457973b
eleven.krsk <eleven.krsk@gmail.com>
2017-12-22 11:32:13 -0500

    ready rating, price, username and heart in card

--- js/cards.js
+++ js/cards.js
@@ -5,6 +5,11 @@ var cards_dashboard = new Vue({
       image: 'https://source.unsplash.com/random/800x600',
       username_status: ['online', 'offline'],
       username: 'userName',
+      
+      rating: {
+        counter: 5.0,
+        quantity: 3301
+      },
       price: 'от 500 '
     }
   }
