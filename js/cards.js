a18998eb7afc7cb34252687c905935d6869a88de
eleven.krsk <eleven.krsk@gmail.com>
2017-12-22 11:18:23 -0500

    Make pre-template for cards. Bug: personal grid system (for 5 lines) makes conflict for internal grid

--- js/cards.js
+++ js/cards.js
@@ -1,6 +1,11 @@
 var cards_dashboard = new Vue({
   el: '#cards_dashboard',
   data: {
-    message: 'Hello Vue!'
+    card: {
+      image: 'https://source.unsplash.com/random/800x600',
+      username_status: ['online', 'offline'],
+      username: 'userName',
+      price: 'от 500 '
+    }
   }
 })
