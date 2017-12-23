b89d6a45f63c09332e6cf22767ea7dd72b24ca9f
Pavel Liferenko <2kie.co@gmail.com>
2017-12-23 21:38:17 +0000

    slider is alive! :)

--- js/cards.js
+++ js/cards.js
@@ -6,9 +6,19 @@ var cards_dashboard = new Vue({
     taskcard: {
       template: '#taskcard-template'
     },
-  },
+      card: {
+      image: 'https://source.unsplash.com/random/800x600',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+      username_status: ['online', 'offline'],
+      username: 'userName',
+      
+      rating: {
+        counter: '5.0',
+        quantity: 260
+      },
+      price: 'от 500 '
+    }},
   data: {
 
   }
-  
 })
