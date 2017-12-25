56cc517c1881dbe1db1fbbc24962765c45ae7cec
Pavel Liferenko <2kie.co@gmail.com>
2017-12-25 16:12:35 +0000

    working templates slide and cards

--- js/slider.js
+++ js/slider.js
@@ -4,22 +4,23 @@ var slide_dashboard = new Vue({
   el: '#slide_dashboard',
   components: {
     slidecard: {
-      template: '#slide-template'
+      template: '#slide-template',
+      data: function() {
+        return {
+          active: false,
+          slide: {
+            image: 'https://source.unsplash.com/random/800x600?count=11',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+            username_status: ['online', 'offline'],
+            username: 'userName',
+            rating: {
+              counter: '5.0',
+              quantity: 260
+            },
+            price: 'от 500 '
+          }}}
     },
-      card: {
-      image: 'https://source.unsplash.com/random/800x600',
-      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
-      username_status: ['online', 'offline'],
-      username: 'userName',
-      
-      rating: {
-        counter: '5.0',
-        quantity: 260
-      },
-      price: 'от 500 '
-    }},
-  data: {
+},
 
-  }
 })
 
